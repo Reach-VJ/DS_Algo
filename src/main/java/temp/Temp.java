c73794212ad3a5b0a2ccaf8afe5ffda1c39226ed
bachi305 <bachi305@gmail.com>
2024-03-28 15:59:55 -0700

    Updating file

--- src/main/java/temp/Temp.java
+++ src/main/java/temp/Temp.java
@@ -2,8 +2,6 @@ package temp;
 
 public class Temp {
     public static void main(String [] args) {
-        while(1==1) {
-            System.out.println("Hello");
-        }
+        System.out.println("Hello");
     }
 }
