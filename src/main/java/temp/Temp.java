e9ddeb3fed08f4590d5c05bd95b42622bbcd843b
bachi305 <bachi305@gmail.com>
2024-03-31 06:05:04 -0700

    hello world

--- src/main/java/temp/Temp.java
+++ src/main/java/temp/Temp.java
@@ -4,6 +4,6 @@ public class Temp {
     public static void main(String [] args) {
         System.out.println("Hello World, Welcome to coding world");
         System.out.println("R1 changes");
-        System.out.println("Main changes");
+        System.out.println("Main changes Main changes");
     }
 }
