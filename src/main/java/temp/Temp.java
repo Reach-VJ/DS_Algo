25b36458490ab51693c65f75f7659d351a8d3568
bachi305 <bachi305@gmail.com>
2024-03-31 05:03:26 -0700

    Adding R1 changes

--- src/main/java/temp/Temp.java
+++ src/main/java/temp/Temp.java
@@ -3,5 +3,6 @@ package temp;
 public class Temp {
     public static void main(String [] args) {
         System.out.println("Hello World, Welcome to coding world");
+        System.out.println("It includes R1 release");
     }
 }
