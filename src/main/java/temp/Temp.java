022634e588f1955334ef2492f4e89b233d6e61e2
bachi305 <bachi305@gmail.com>
2024-03-31 05:16:53 -0700

    Revert "Updating R1 changes stash@2 due to some conflict"
    This reverts commit 03521e112cca265c403ef6f5bbd37da059fa3a14.

--- src/main/java/temp/Temp.java
+++ src/main/java/temp/Temp.java
@@ -3,6 +3,5 @@ package temp;
 public class Temp {
     public static void main(String [] args) {
         System.out.println("Hello World, Welcome to coding world");
-        System.out.println("R1 changes");
     }
 }
